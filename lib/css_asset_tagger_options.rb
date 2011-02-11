c9ee5154e537a5e1f43d4f290597e59ecbf3bd06
Andrew Kaspick <andrew@redlinesoftware.com>
2011-02-11 04:38:13 -0600

    clean up settings module for easier customization and update README

--- lib/css_asset_tagger_options.rb
+++ lib/css_asset_tagger_options.rb
@@ -1,3 +1,12 @@
 module CssAssetTaggerOptions
   mattr_accessor :perform_tagging, :css_paths, :asset_path, :show_warnings
+
+  @@perform_tagging = Rails.env.production?
+  @@css_paths = %W(#{Rails.root}/public/stylesheets)
+  @@asset_path = Rails.root.join('public')
+  @@show_warnings = Rails.env.development?
+
+  def self.setup
+    yield self
+  end
 end
