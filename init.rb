0dd767187e908093ace1072cf0bcdb13acc9dbe3
Andrew Kaspick <andrew@redlinesoftware.com>
2011-08-02 10:48:48 -0500

    remove deprected RAILS_ROOT constant

--- init.rb
+++ init.rb
@@ -2,8 +2,8 @@
 
 config.to_prepare do
   CssAssetTaggerOptions.perform_tagging = Rails.env.production?
-  CssAssetTaggerOptions.css_paths = %W(#{RAILS_ROOT}/public/stylesheets)
-  CssAssetTaggerOptions.asset_path = "#{RAILS_ROOT}/public"
+  CssAssetTaggerOptions.css_paths = %W(#{Rails.root}/public/stylesheets)
+  CssAssetTaggerOptions.asset_path = "#{Rails.root}/public"
   CssAssetTaggerOptions.show_warnings = Rails.env.development?
 
   CssAssetTagger.tag(CssAssetTaggerOptions.css_paths) if CssAssetTaggerOptions.perform_tagging
