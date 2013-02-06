72261e080b43d5c34968030a25a064001d1df73b
Andrew Kaspick <andrew@redlinesoftware.com>
2013-02-05 20:35:37 -0600

    fix tests so they pass

--- lib/css_asset_tagger_options.rb
+++ lib/css_asset_tagger_options.rb
@@ -1,10 +1,12 @@
 module CssAssetTaggerOptions
   mattr_accessor :perform_tagging, :css_paths, :asset_path, :show_warnings
 
-  @@perform_tagging = Rails.env.production?
-  @@css_paths = %W(#{Rails.root}/public/stylesheets)
-  @@asset_path = Rails.root.join('public')
-  @@show_warnings = Rails.env.development?
+  if defined?(Rails)
+    @@perform_tagging = Rails.env.production?
+    @@css_paths = %W(#{Rails.root}/public/stylesheets)
+    @@asset_path = Rails.root.join('public')
+    @@show_warnings = Rails.env.development?
+  end
 
   def self.setup
     yield self
--- test/css_asset_tagger_test.rb
+++ test/css_asset_tagger_test.rb
@@ -1,15 +1,17 @@
-require 'test/unit'
-require 'erb'
 require 'rubygems'
-require 'active_support'
-require File.dirname(__FILE__)+'/../lib/css_asset_tagger_options'
-require File.dirname(__FILE__)+'/../lib/css_asset_tagger'
+
+require 'test/unit'
+require 'active_support/all'
+
+require 'css_asset_tagger'
+require 'css_asset_tagger_options'
 
 class CssAssetTaggerTest < Test::Unit::TestCase
   ASSET_PATH = File.dirname(__FILE__)+'/assets'
   TEST_STYLESHEET = ASSET_PATH+'/stylesheets/test.css'
 
   def setup
+    CssAssetTaggerOptions.perform_tagging = true
     CssAssetTaggerOptions.asset_path = [ASSET_PATH]
     FileUtils.cp TEST_STYLESHEET+'.template', TEST_STYLESHEET
   end
@@ -29,7 +31,7 @@ class CssAssetTaggerTest < Test::Unit::TestCase
     # set the timestamps for the relative and absolute images
     abs_time = File.stat(ASSET_PATH+'/images/test.png').mtime.to_i
     rel_time = File.stat(ASSET_PATH+'/stylesheets/images/test.png').mtime.to_i
-    
+
     assert_equal ERB.new(File.read(TEST_STYLESHEET+'.expected')).result(self.send(:binding)).to_s, File.read(TEST_STYLESHEET).to_s
   end
 end
