d4eb952bb86525622d7177bb3ac1fcb2c465aa3f
Andrew Kaspick <andrew@redlinesoftware.com>
2013-02-05 18:16:23 -0600

    move the to_prepare block from the old plugin init.rb to the railtie

--- lib/css_asset_tagger/railtie.rb
+++ lib/css_asset_tagger/railtie.rb
@@ -5,5 +5,9 @@ module CssAssetTaggerOptions
     rake_tasks do
       Dir[File.expand_path('../../tasks/*.rake', __FILE__)].each { |f| load f }
     end
+
+    config.to_prepare do
+      CssAssetTagger.tag(CssAssetTaggerOptions.css_paths) if CssAssetTaggerOptions.perform_tagging
+    end
   end
 end
